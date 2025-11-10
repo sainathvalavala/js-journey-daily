1b434546b4418ea73a0d042688e61825d81b6cb8
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-11-10 22:53:45 +0530

    updated

--- javaScript/ES6/const.js
+++ javaScript/ES6/const.js
@@ -1,10 +1,13 @@
-const x=100;
+// const a;
+// console.log(a)-- syntax error: missing initializer in const declaration.
+
+// const x=100;
 // const y; --Uncaught SyntaxError: Missing initializer in const declaration
-const y=300;
+// const y=300;
 // y=13--Uncaught TypeError: Assignment to constant variable.
 const ar=[1,2,3]//array have reference , we are not changing the refrence but changling the value
-// ar[1]=4;
-// console.log(ar)
+ar[1]=4;
+console.log(ar)
 //freeze- cant modify array elements
 // br=[5,6,7]
 // Object.freeze(br);
@@ -12,7 +15,7 @@ const ar=[1,2,3]//array have reference , we are not changing the refrence but ch
 // console.log(br)
 
 //seal- cant extend array
-br=[5,6,7]
-Object.seal(br);
-br[2]=("ravi")
-console.log(br)
+// br=[5,6,7]
+// Object.seal(br);
+// br[2]=("ravi")
+// console.log(br)
---
+++ javaScript/arrow functions/first.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  return a + b;
+}
+console.log(add(1, 2));
+
+var sub = function (a, b) {
+  return a - b;
+};
+console.log(sub(5, 2));
+
+var mul = (a, b) => a * b;
+console.log(mul(10, 2));
+var xyz = (a) => a * a;
+console.log(xyz(5));
